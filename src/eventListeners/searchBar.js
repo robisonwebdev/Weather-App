fdf3fc032c70d73ee1c7d9b68f36f3da9e4e35fb
David Robison <david@robisonweb.dev>
2021-04-16 13:26:52 -0600

    Called displayLocationData()

--- src/eventListeners/searchBar.js
+++ src/eventListeners/searchBar.js
@@ -1,11 +1,13 @@
 import getLocationData from '../getLocationData';
 import clearSearch from '../clearSearch';
+import displayLocationData from '../displayLocationData';
 
 function searchBar(target) {
     target.addEventListener('keydown', async (event) => {
         if (event.key === 'Enter') {
             await getLocationData(target.value);
             clearSearch();
+            displayLocationData();
         }
     })
 }
