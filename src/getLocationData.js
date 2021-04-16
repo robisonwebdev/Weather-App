d60b99ac4c2d35a783935de9573fc45d3b949391
David Robison <david@robisonweb.dev>
2021-04-16 13:27:22 -0600

    Push data to locationData

--- src/getLocationData.js
+++ src/getLocationData.js
@@ -1,10 +1,14 @@
+import locationData from "./storedLocationData";
+
 async function getLocationData(cityName) {
     const api = '42e98cfcab5534e4a21196b5a280a360';
 
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${api}`, { mode: 'cors' });
     const cityData = await response.json();
 
-    console.log(`Temp: ${Math.round(cityData.main.temp)}`);
+    console.log(cityData);
+    locationData.push(cityData);
+    console.log(locationData);
 }
 
 export default getLocationData;
