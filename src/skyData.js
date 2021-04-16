1c6cf1ce88815e46a09684feb610eca7d694fcd8
David Robison <david@robisonweb.dev>
2021-04-16 13:27:50 -0600

    Created function to display current sky data

---
+++ src/skyData.js
@@ -0,0 +1,16 @@
+import locationData from "./storedLocationData";
+
+function skyData() {
+    const skyData = document.createElement('div');
+    const h2 = document.createElement('h2');
+
+    skyData.setAttribute('id', 'skyData');
+
+    h2.innerText = locationData[0]['weather'][0].main;
+
+    skyData.appendChild(h2);
+
+    return skyData;
+}
+
+export default skyData;
