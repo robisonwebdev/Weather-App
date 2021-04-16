37228141af622d2b12eaa94178242d13d4d2abb2
David Robison <david@robisonweb.dev>
2021-04-16 13:28:42 -0600

    Commented out displayLocationData

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import displayLocationData from "./displayLocationData";
+
 function main() {
     const main = document.createElement('div');
 
     main.setAttribute('id', 'main');
+
+    // main.appendChild(displayLocationData());
 
     return main;
 }
