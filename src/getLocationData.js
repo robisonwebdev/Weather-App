7fcb3fed87b047ff51386393eb300326c41ed0b7
David Robison <david@robisonweb.dev>
2021-04-16 08:53:09 -0600

    Reworked function to use async

--- src/getLocationData.js
+++ src/getLocationData.js
@@ -1,18 +1,10 @@
-function getLocationData(cityName) {
-    let api = '42e98cfcab5534e4a21196b5a280a360';
+async function getLocationData(cityName) {
+    const api = '42e98cfcab5534e4a21196b5a280a360';
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${api}`, { mode: 'cors' })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
-            console.log(`Temp: ${response.main.temp}`);
-            console.log(`Skies: ${response.weather[0].description}`);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${api}`, { mode: 'cors' });
+    const cityData = await response.json();
+
+    console.log(`Temp: ${Math.round(cityData.main.temp)}`);
 }
 
 export default getLocationData;
