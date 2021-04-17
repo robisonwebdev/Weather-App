0cfa72a90964a3ff88d81553c8ea7fdd24c89a34
David Robison <david@robisonweb.dev>
2021-04-17 10:05:01 -0600

    Called cleareLocationData, deleteStoredLocationData, and displayLocationData

--- src/eventListeners/searchButton.js
+++ src/eventListeners/searchButton.js
@@ -1,12 +1,18 @@
-import clearSearch from "../clearSearch";
-import getLocationData from "../getLocationData";
+import getLocationData from '../getLocationData';
+import clearSearch from '../clearSearch';
+import displayLocationData from '../displayLocationData';
+import clearLocationData from '../clearLocationData';
+import deleteStoredLocationData from '../deleteStoredLocationData';
 
 function searchButton(target) {
     target.addEventListener('click', async () => {
         const searchBar = document.querySelector('#search');
 
+        deleteStoredLocationData();
         await getLocationData(searchBar.value);
         clearSearch();
+        clearLocationData();
+        displayLocationData();
     })
 }
 
