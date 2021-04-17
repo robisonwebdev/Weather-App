6fb94e86c1dcb504a72823cd1b4939949880b020
David Robison <david@robisonweb.dev>
2021-04-17 10:04:22 -0600

    Called cleareLocationData and deleteStoredLocationData

--- src/eventListeners/searchBar.js
+++ src/eventListeners/searchBar.js
@@ -1,12 +1,16 @@
 import getLocationData from '../getLocationData';
 import clearSearch from '../clearSearch';
 import displayLocationData from '../displayLocationData';
+import clearLocationData from '../clearLocationData';
+import deleteStoredLocationData from '../deleteStoredLocationData';
 
 function searchBar(target) {
     target.addEventListener('keydown', async (event) => {
         if (event.key === 'Enter') {
+            deleteStoredLocationData();
             await getLocationData(target.value);
             clearSearch();
+            clearLocationData();
             displayLocationData();
         }
     })
